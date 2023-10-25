82b6eb97736e22b470ba2f787339444f9db5b4a1
Fernando Link <fernandolink@gmail.com>
2023-10-25 15:19:51 -0300

    funcional

--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -1,32 +1,34 @@
 package br.com.alura.orgs.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
+import br.com.alura.orgs.dao.ProdutosDao
 import br.com.alura.orgs.model.Produto
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
-            Produto(nome = "teste",
-                descricao = "teste desc",
-                valor = BigDecimal("19.99")
-            ),
-            Produto(nome = "teste 1",
-                descricao = "teste desc 1",
-                valor = BigDecimal("29.99")
-            ),
-            Produto(nome = "teste 2",
-                descricao = "teste desc 2",
-                valor = BigDecimal("39.99")
-            ),
-        ))
+        val dao = ProdutosDao()
+        recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = dao.buscaTodos())
+
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener {
+            val intent = Intent(this, FormularioProdutoActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
